a5783f9d064d1074087677dbbf797a2160b6ecc7
Bobby Krupczak <rdk@northside.krupczak.org>
2023-09-05 16:24:15 -0400

    Added droneModels as git submodule; removed hardcoded version.  Now load from git submodule version.  Also, add DroneParams search functions to handle name collision.

--- OpenAthenaIOS/DroneParams.swift
+++ OpenAthenaIOS/DroneParams.swift
@@ -23,6 +23,22 @@ public struct DroneCCDInfo {
     let widthPixels: Double
     let heightPixels: Double
     let comment: String
+    let isThermal: Bool
+    let lensType: String // perspective or fisheye
+    let radialR1: Double // R1,2,3, T1,T2: only if perspective
+    let radialR2: Double
+    let radialR3: Double
+    let tangentialT1: Double
+    let tangentialT2: Double
+    let c: Double        // c,d,e,f: only if fisheye
+    let d: Double
+    let e: Double
+    let f: Double
+    let poly0: Double // only for fisheye lens
+    let poly1: Double
+    let poly2: Double
+    let poly3: Double
+    let poly4: Double
 }
 
 public enum DroneParamsError: String, Error {
@@ -34,12 +50,28 @@ public enum DroneParamsError: String, Error {
 public class DroneParams
 {
     var droneParamsLastUpdate = Date()
-    var droneParamsDate: Date?
+    var droneParamsDate: String?
     var droneCCDParams = [
         // mavic pro
         DroneCCDInfo(makeModel: "djiFC220", ccdWidthMMPerPixel: 6.17/4000.0,
                                  ccdHeightMMPerPixel: 4.55/3000.0, widthPixels: 4000.0,
-                                 heightPixels: 3000.0, comment: ""),
+                                 heightPixels: 3000.0, comment: "",
+                     isThermal: false,
+                     lensType: "persective",
+                     radialR1: 0.032984,
+                     radialR2: -0.085597,
+                     radialR3: 0.0777778,
+                     tangentialT1: 0.000409773,
+                     tangentialT2: -0.000806846,
+                     c: 0,
+                     d: 0,
+                     e: 0,
+                     f: 0,
+                     poly0: 0,
+                     poly1: 0,
+                     poly2: 0,
+                     poly3: 0,
+                     poly4: 0)
     ]
     
     // init with baked in hardcoded values loaded from
@@ -54,15 +86,22 @@ public class DroneParams
             let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
             //print("Json obj is: \(jsonObject)")
             if let dictionary = jsonObject as? [String: Any] {
-                print("Dictionary created!")
-                let dateFormatter = DateFormatter()
-                let fileDate = convertUnixDateOutputToDate(unixDateOutput: dictionary["lastUpdate"] as! String)
+                print("Drone models dictionary created!")
+                //let dateFormatter = DateFormatter()
+                //let fileDate = convertUnixDateOutputToDate(unixDateOutput: dictionary["lastUpdate"] as! String)
+                
+                let fileDate = dictionary["lastUpdate"] as! String
+                
                 let anArray = convertNSArray(array: dictionary["droneCCDParams"] as! NSArray)
+                
                 if !anArray.isEmpty {
                     print("Setting drone ccd params array \(fileDate)")
                     droneParamsDate = fileDate
                     droneCCDParams = anArray
                     print("\(droneCCDParams.count) drone ccd entries")
+                }
+                else {
+                    print("Empty drone ccd params array \(fileDate)")
                 }
             }
             else {
@@ -77,6 +116,54 @@ public class DroneParams
     // init with local data file
     // init(file: dataFile)
     // func readDataFile
+    
+    // get matching drones; many drone models have make/model collision between
+    // main color camera and their secondary thermal camera; this function returns
+    // array of matches and caller will have to further match based on pixel width or
+    // other distinguishing parameter
+    
+    func getMatchingDrones(makeModel: String) throws -> [DroneCCDInfo]
+    {
+        var matchingDrones: [DroneCCDInfo] = []
+        
+        for drone in droneCCDParams {
+            if drone.makeModel.lowercased() == makeModel.lowercased() {
+                matchingDrones.append(drone)
+            }
+        }
+        
+        if matchingDrones.isEmpty {
+            throw DroneParamsError.droneNotFound
+        }
+        
+        return matchingDrones
+    }
+    
+    // given an image width, find the make/model drone with the
+    // closest match
+    func getMatchingDrone(makeModel: String, targetWidth: Double) throws -> DroneCCDInfo?
+    {
+        var theDrones: [DroneCCDInfo]
+        var smallestDifference = Double.infinity
+        var closestDrone: DroneCCDInfo?
+        var difference: Double
+                
+        try theDrones = getMatchingDrones(makeModel: makeModel)
+        
+        for drone in theDrones {
+            difference = fabs(drone.widthPixels - targetWidth)
+            if difference < smallestDifference {
+                closestDrone = drone
+                smallestDifference = difference
+            }
+        }
+        
+        if closestDrone == nil {
+            throw DroneParamsError.droneNotFound
+        }
+        
+        return closestDrone 
+    }
 
     // given makeModel, find drone ccd info
     // convert all makeModel strings to lowercase 
@@ -101,13 +188,13 @@ public class DroneParams
     }
     
     // written by ChatGPT; fixed to add am/pm marker "a"
-    private func convertUnixDateOutputToDate(unixDateOutput: String) -> Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEE MMM dd HH:mm:ss a zzz yyyy"
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        
-        return dateFormatter.date(from: unixDateOutput)
-    }
+//    private func convertUnixDateOutputToDate(unixDateOutput: String) -> Date? {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "EEE MMM dd HH:mm:ss a zzz yyyy"
+//        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+//
+//        return dateFormatter.date(from: unixDateOutput)
+//    }
     
     // written by ChatGPT; convert NSArray to an array of structures
     private func convertNSArray(array: NSArray) -> [DroneCCDInfo]
@@ -119,16 +206,51 @@ public class DroneParams
                let ccdHeightMMPerPixel = dictionary["ccdHeightMMPerPixel"] as? String,
                let widthPixels = dictionary["widthPixels"] as? Double,
                let heightPixels = dictionary["heightPixels"] as? Double,
+               let isThermal = dictionary["isThermal"] as? Bool,
+               let lensType = dictionary["lensType"] as? String,
+               let radialR1 = dictionary["radialR1", default: 0.0] as? Double,
+               let radialR2 = dictionary["radialR2", default: 0.0] as? Double,
+               let radialR3 = dictionary["radialR3", default: 0.0] as? Double,
+               let tangentialT1 = dictionary["tangentialT1", default: 0.0] as? Double,
+               let tangentialT2 = dictionary["tangentialT2", default: 0.0] as? Double,
+               let c = dictionary["c", default: 0.0] as? Double,
+               let d = dictionary["d", default: 0.0] as? Double,
+               let e = dictionary["e", default: 0.0] as? Double,
+               let f = dictionary["f", default: 0.0] as? Double,
+               let poly0 = dictionary["poly0", default: 0.0] as? Double,
+               let poly1 = dictionary["poly1", default: 0.0] as? Double,
+               let poly2 = dictionary["poly2", default: 0.0] as? Double,
+               let poly3 = dictionary["poly3", default: 0.0] as? Double,
+               let poly4 = dictionary["poly4", default: 0.0] as? Double,
                let comment = dictionary["comment"] as? String {
+                
                 let w = convertDivisonString(str: ccdWidthMMPerPixel)
                 let h = convertDivisonString(str: ccdHeightMMPerPixel)
+                
                 return DroneCCDInfo(
                     makeModel: makeModel,
                     ccdWidthMMPerPixel: w,
                     ccdHeightMMPerPixel: h,
                     widthPixels: widthPixels,
                     heightPixels: heightPixels,
-                    comment: comment)
+                    comment: comment,
+                    isThermal: isThermal,
+                    lensType: lensType,
+                    radialR1: radialR1,
+                    radialR2: radialR2,
+                    radialR3: radialR3,
+                    tangentialT1: tangentialT1,
+                    tangentialT2: tangentialT2,
+                    c: c,
+                    d: d,
+                    e: e,
+                    f: f,
+                    poly0: poly0,
+                    poly1: poly1,
+                    poly2: poly2,
+                    poly3: poly3,
+                    poly4: poly4
+                )
             }
             return nil
         }
