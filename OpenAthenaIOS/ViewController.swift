7bf6c5dfb3d123ecfef38da13c48b9e22ba2349d
Bobby Krupczak <rdk@northside.krupczak.org>
2023-09-13 09:29:17 -0400

    Checkpoint before CoT feature branch.

--- OpenAthenaIOS/ViewController.swift
+++ OpenAthenaIOS/ViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var app: AppDelegate = UIApplication.shared.delegate as! AppDelegate
-    var version: Float = 1.20
+    var version: Float = 1.2111
     @IBOutlet var textView: UITextView!
     @IBOutlet var imageView: UIImageView!
     var dem: DigitalElevationModel?
